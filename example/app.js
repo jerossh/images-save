3d0a75237cf72acf3933778ca3a354fcc31c9a7b
jerossh <jerosshen@gmail.com>
2017-05-24 00:12:48 +0800

    有必要自己去了解一下图片怎么优化了，先放弃图片优化

--- example/app.js
+++ example/app.js
@@ -6,12 +6,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const multipart    = require('connect-multiparty');
+const del = require('del');
 const debug = require('debug')('app');
 const imgSave = require('../');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(multipart());
 
+// 删除文件夹
+del(['public/upload']).then(paths => {
+    console.log('Deleted files and folders:\n', paths.join('\n'));
+});
 
 app.post('/upload', function (req, res) {
     // debug('上传了', req.files);
